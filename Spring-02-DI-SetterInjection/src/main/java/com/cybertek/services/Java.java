3471a231b2787211e0a39c2978f1d6ac721ad743
Azamat Kulov <aza.kulov@gmail.com>
2020-12-02 21:56:55 -0800

    DI-Setter Injection

--- Spring-02-DI-SetterInjection/src/main/java/com/cybertek/services/Java.java
+++ Spring-02-DI-SetterInjection/src/main/java/com/cybertek/services/Java.java
@@ -1,20 +1,27 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSession;
+import lombok.Data;
 
+//@Getter
+//@Setter
+@Data
 public class Java implements Course {
 
-    private OfficeHours officeHours;
-
-    public OfficeHours getOfficeHours() {
-        return officeHours;
+    private ExtraSession extraSession;
+/*
+    public ExtraSession getExtraSession() {
+        return extraSession;
     }
 
-    public void setOfficeHours(OfficeHours officeHours) {
-        this.officeHours = officeHours;
+    public void setExtraSession(ExtraSession extraSession) {
+        this.extraSession = extraSession;
     }
+
+ */
 
     public void getTeachingHours() {
-          System.out.println("Weekly Teaching Hours: " + (20 + officeHours.getHours()));
+          System.out.println("Weekly Teaching Hours: " + (20 + extraSession.getHours()));
     }
 }
--- Spring-02-DI-SetterInjection/src/main/java/com/cybertek/services/Selenium.java
+++ Spring-02-DI-SetterInjection/src/main/java/com/cybertek/services/Selenium.java
@@ -1,14 +1,15 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSession;
 import lombok.Data;
 
 @Data
 public class Selenium implements Course {
 
-    private OfficeHours officeHours;
+    private ExtraSession extraSession;
 
     public void getTeachingHours() {
-        System.out.println("Weekly Teaching Hours: " + (15 + officeHours.getHours()));
+        System.out.println("Weekly Teaching Hours: " + (15 + extraSession.getHours()));
     }
 }
