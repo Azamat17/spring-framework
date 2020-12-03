7f01dee63826fe652c2c38e92ea15c1ac84e408c
Azamat Kulov <aza.kulov@gmail.com>
2020-12-02 21:46:04 -0800

    Refactor CybertekApp

--- Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
@@ -1,17 +1,18 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSession;
 
 public class Java implements Course {
 
-    OfficeHours officeHours;
+    ExtraSession extraSession;
 
-    public Java(OfficeHours officeHours) {
-        this.officeHours = officeHours;
+    public Java(ExtraSession extraSession) {
+        this.extraSession = extraSession;
     }
 
     @Override
     public void getTeachingHours() {
-          System.out.println("Weekly Teaching Hours: " + (20 + officeHours.getHours()));
+          System.out.println("Weekly Teaching Hours: " + (20 + extraSession.getHours()));
     }
 }
