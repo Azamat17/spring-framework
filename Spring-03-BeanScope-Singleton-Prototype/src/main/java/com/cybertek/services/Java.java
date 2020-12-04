eb2e8eaf17cf50260d4c33b4ca3c08434bac08a7
Azamat Kulov <aza.kulov@gmail.com>
2020-12-03 19:55:31 -0800

    Bean Scope

--- Spring-03-BeanScope-Singleton-Prototype/src/main/java/com/cybertek/services/Java.java
+++ Spring-03-BeanScope-Singleton-Prototype/src/main/java/com/cybertek/services/Java.java
@@ -1,11 +1,7 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
-import lombok.Data;
 
-//@Getter
-//@Setter
-@Data
 public class Java implements Course {
 
     public void getTeachingHours() {
